3bf91b318be0de5868d805623b0cdf869716af96
benwsheridan <ben.w.sheridan@gmail.com>
2024-04-03 17:04:54 +0100

    comments addded

--- Multi-Tool.py
+++ Multi-Tool.py
@@ -173,7 +173,7 @@ except Exception as e:
 
 #ipWidth = calculate_button_width('Get IP')
 ipButton = tk.CTkButton(app, text="Get IP\n(Needs User Input)", command=user_choice, width=button_width, height=button_height)
-ipButton.grid(row=0, column=0, padx=10, pady=10, sticky='nswe')
+ipButton.grid(row=0, column=0, padx=10, pady=10, sticky='nswe') # Sticky means that it will stick to North, South, West and East (nswe) of the column its in
 
 hwidButton = tk.CTkButton(app, text="Get HWID", command=get_hwid, width=button_width, height=button_height)
 hwidButton.grid(row=0, column=1, padx=10, pady=10, sticky='nswe')
